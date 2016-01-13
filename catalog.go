682045742d1262da4950338b0b735d66515cd637
Dave Protasowski <dprotaso@gmail.com>
2016-01-13 12:07:30 -0500

    Update catalog structs to match Service Broker API v2.8
    Signed-off-by: Topher Bullock <cbullock@pivotal.io>

--- catalog.go
+++ catalog.go
@@ -1,42 +1,51 @@
 package brokerapi
 
 type Service struct {
-	ID            string          `json:"id"`
-	Name          string          `json:"name"`
-	Description   string          `json:"description"`
-	Bindable      bool            `json:"bindable"`
-	PlanUpdatable bool            `json:"plan_updateable"`
-	Plans         []ServicePlan   `json:"plans"`
-	Metadata      ServiceMetadata `json:"metadata"`
-	Tags          []string        `json:"tags"`
+	ID              string                  `json:"id"`
+	Name            string                  `json:"name"`
+	Description     string                  `json:"description"`
+	Bindable        bool                    `json:"bindable"`
+	Tags            []string                `json:"tags,omitempty"`
+	PlanUpdatable   bool                    `json:"plan_updateable"`
+	Plans           []ServicePlan           `json:"plans"`
+	Metadata        *ServiceMetadata        `json:"metadata,omitempty"`
+	DashboardClient *ServiceDashboardClient `json:"dashboard_client,omitempty"`
+}
+
+type ServiceDashboardClient struct {
+	ID          string `json:"id"`
+	Secret      string `json:"secret"`
+	RedirectURI string `json:"redirect_uri"`
 }
 
 type ServicePlan struct {
-	ID          string              `json:"id"`
-	Name        string              `json:"name"`
-	Description string              `json:"description"`
-	Metadata    ServicePlanMetadata `json:"metadata"`
+	ID          string               `json:"id"`
+	Name        string               `json:"name"`
+	Description string               `json:"description"`
+	Free        *bool                `json:"free,omitempty"`
+	Metadata    *ServicePlanMetadata `json:"metadata,omitempty"`
 }
 
 type ServicePlanMetadata struct {
-	Bullets     []string `json:"bullets"`
-	DisplayName string   `json:"displayName"`
+	DisplayName string        `json:"displayName,omitempty"`
+	Bullets     []string      `json:"bullets,omitempty"`
+	Costs       []ServiceCost `json:"costs,omitempty"`
+}
+
+type ServiceCost struct {
+	Amount map[string]float64 `json:"amount"`
+	Unit   string             `json:"unit"`
 }
 
 type ServiceMetadata struct {
-	DisplayName      string                  `json:"displayName"`
-	LongDescription  string                  `json:"longDescription"`
-	DocumentationUrl string                  `json:"documentationUrl"`
-	SupportUrl       string                  `json:"supportUrl"`
-	Listing          ServiceMetadataListing  `json:"listing"`
-	Provider         ServiceMetadataProvider `json:"provider"`
+	DisplayName         string `json:"displayName,omitempty"`
+	ImageUrl            string `json:"imageUrl,omitempty"`
+	LongDescription     string `json:"longDescription,omitempty"`
+	ProviderDisplayName string `json:"providerDisplayName,omitempty"`
+	DocumentationUrl    string `json:"documentationUrl,omitempty"`
+	SupportUrl          string `json:"supportUrl,omitempty"`
 }
 
-type ServiceMetadataListing struct {
-	Blurb    string `json:"blurb"`
-	ImageUrl string `json:"imageUrl"`
-}
-
-type ServiceMetadataProvider struct {
-	Name string `json:"name"`
+func FreeValue(v bool) *bool {
+	return &v
 }
